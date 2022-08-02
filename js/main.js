0cadfc557703f2aac395ed714b191d974335453a
Oswaldo Alonzo <oalonzo@gmail.com>
2022-08-02 10:11:40 -0600

    agregando funcion seleccionarMascotaEnemigo

--- js/main.js
+++ js/main.js
@@ -7,22 +7,47 @@ function seleccionarMascotaJugador() {
   let inputLangostelvis = document.getElementById("langostelvis");
   let inputTucapalma = document.getElementById("tucapalma");
   let inputPydos = document.getElementById("pydos");
+  let spanMascotaJugador = document.getElementById("mascota-jugador");
 
   if (inputHipodoge.checked) {
-    alert("Seleccionaste a hipodoge");
+    spanMascotaJugador.innerHTML = "Hipodoge";
   } else if (inputCapipepo.checked) {
-    alert("Seleccionaste a capipepo");
+    spanMascotaJugador.innerHTML = "Capipepo";
   } else if (inputRatigueya.checked) {
-    alert("Seleccionaste a ratigueya");
+    spanMascotaJugador.innerHTML = "Ratigueya";
   } else if (inputLangostelvis.checked) {
-    alert("Seleccionaste a langostelvis");
+    spanMascotaJugador.innerHTML = "Langostelvis";
   } else if (inputTucapalma.checked) {
-    alert("Seleccionaste a tucapalma");
+    spanMascotaJugador.innerHTML = "Tucapalma";
   } else if (inputPydos.checked) {
-    alert("Seleccionaste a pydos");
+    spanMascotaJugador.innerHTML = "Pydos";
   } else {
     alert("No seleccionaste ninguna mascota");
   }
+  seleccionarMascotaEnemigo();
+}
+
+function seleccionarMascotaEnemigo() {
+  let ataqueAleatorio = aleatorio(1, 6);
+  let spanMascotaEnemigo = document.getElementById("mascota-enemigo");
+
+  if (ataqueAleatorio == 1) {
+    spanMascotaEnemigo.innerHTML = "Hipodoge";
+  } else if (ataqueAleatorio == 2) {
+    spanMascotaEnemigo.innerHTML = "Capipepo";
+  } else if (ataqueAleatorio == 3) {
+    spanMascotaEnemigo.innerHTML = "Ratigueya";
+  } else if (ataqueAleatorio == 4) {
+    spanMascotaEnemigo.innerHTML = "Langostelvis";
+  } else if (ataqueAleatorio == 5) {
+    spanMascotaEnemigo.innerHTML = "Tucapalma";
+  } else if (ataqueAleatorio == 6) {
+    spanMascotaEnemigo.innerHTML = "Pydos";
+  }
+}
+
+function aleatorio(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 function inicio() {
